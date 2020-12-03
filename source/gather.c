c3a30c6c0f633c1a91c2ec8d66d6e241c26637cc
JuliaMowrey <60298256+JuliaMowrey@users.noreply.github.com>
2020-12-03 08:25:37 -0500

    Update gather.c

--- source/gather.c
+++ source/gather.c
@@ -5,7 +5,7 @@ int main(int argc, char** argv) {
   int size;
   int my_rank; 
   int sendbuf[2];
-  int recvbuf[8] = {-1,-1,-1,-1,-1,-1,-1,-1};
+  int recvbuf[8] = {0,1,2,3,4,5,6,7};
   int i;
     
   MPI_Init(&argc, &argv);
